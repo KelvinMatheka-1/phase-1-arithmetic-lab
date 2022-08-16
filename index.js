4c4784365358afa3c405eda8d83cd0bbb61ab832
Kelvin M Matheka <beingmatheka@gmail.com>
2022-08-16 09:22:05 +0300

    Done with assignnment

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const num1 = 31
 const num2 = 2
 
-function multiply(num1, num2){
-  console.log(num1 * num2);
-};
+const multiply = num1 * num2
+
+const random = 1
+const variable = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 8
+const num4 = 2
+
+const mod = num3 / num4
 
